a1ce107f43108c00691f880bbecf7ae9544bc157
gastonpereyra <rgpxen@gmail.com>
2019-03-17 00:24:42 -0300

    Conectar Enrutador con el Server

--- server_api_usuarios.js
+++ server_api_usuarios.js
@@ -3,6 +3,7 @@ const http = require('http');
 const mysql = require('mysql');
 const db = require('./api_usuarios/db_manager');
 const {server_host, server_api_usuarios_port, database_mysql, getFecha} = require('./opciones');
+const router = require('./api_usuarios/router');
 
 // Conectar base de datos MYSQL
 
@@ -10,10 +11,10 @@ const connection = mysql.createConnection(database_mysql);
 db.conectar(connection);
 
 // Crear Server
-const server_api_usuarios = http.createServer((req,res) => {
+const server_api_usuarios = http.createServer( (req,res) => {
     const fecha = getFecha();
     console.log(`API USUARIOS | ${fecha.dia} | ${fecha.hora} | HTTP/${req.httpVersion} | ${req.method} | '${req.url}' `);
-    res.end("SERVER - REST API : USUARIOS");
+    router(connection,req,res);
 });
 
 // Conectar Server y ponerlo a "escuchar"
